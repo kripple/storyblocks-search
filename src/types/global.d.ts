9c64ac3b85de3b4df7cd7ca770eb40d239e48ec1
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-08-27 21:31:23 -0400

    add to global types

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,3 +1,15 @@
+declare type ChangeEvent = import('react').ChangeEvent<HTMLInputElement>;
 declare type FormEvent = import('react').FormEvent<HTMLFormElement>;
 declare type ReactNode = import('react').ReactNode;
 
+declare type ImageResults = ({
+  id: number;
+  title: string;
+  thumbnail_url: string;
+  preview_url: string;
+} & Record<string, string | number | boolean>)[];
+
+declare type ImageResponseData = {
+  total_results: number;
+  results: ImageResults;
+};
